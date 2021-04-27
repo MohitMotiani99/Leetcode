820510bc2a5d1326051f3c946b11c1095dbdc2f1
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-04-27 17:00:50 +0530

    Update 94.cpp

--- 94.cpp
+++ 94.cpp
@@ -12,22 +12,47 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode* > s;
-        if(root==NULL)
-            return {};
-        //s.push(root);
-        TreeNode *curr=root;
-        vector<int> ans;
-        while(curr || !s.empty()){
+//         stack<TreeNode* > s;
+//         if(root==NULL)
+//             return {};
+//         //s.push(root);
+//         TreeNode *curr=root;
+//         vector<int> ans;
+//         while(curr || !s.empty()){
             
-            while(curr){
-                s.push(curr);
-                curr=curr->left;
+//             while(curr){
+//                 s.push(curr);
+//                 curr=curr->left;
+//             }
+//             curr=s.top();
+//             s.pop();
+//             ans.push_back(curr->val);
+//             curr=curr->right;
+//         }
+//         return ans;
+        
+        vector<int> ans;
+        TreeNode * curr=root;
+        while(curr){
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
             }
-            curr=s.top();
-            s.pop();
-            ans.push_back(curr->val);
-            curr=curr->right;
+            else{
+                TreeNode *pre=curr->left;
+                while(pre->right && pre->right!=curr){
+                    pre=pre->right;
+                }
+                if(!pre->right){
+                    pre->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    ans.push_back(curr->val);
+                    pre->right=NULL;
+                    curr=curr->right;
+                }
+            }
         }
         return ans;
     }
